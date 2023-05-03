e9322f6a03b4d8a32006d50085f85ad393ef4a7c
Anderson Wilmsen <ander.wilmsen98@gmail.com>
2023-05-03 20:40:04 -0300

    feat: cria tabela de produtos

---
+++ src/app/shared/infra/db/migrations/1683156837988-CreateTableProducts.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTableProducts1683156837988 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'products',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true,
+                    },
+                    {
+                        name: 'description',
+                        type: 'text',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'price',
+                        type: 'float',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'unit',
+                        type: 'varchar',
+                        length: '100',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'amount',
+                        type: 'float',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'active',
+                        type: 'boolean',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'updated_at',
+                        type: 'timestamp',
+                        isNullable: false,
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('products', true, true, true);
+    }
+}
--- src/app/shared/infra/db/migrations/index.ts
+++ src/app/shared/infra/db/migrations/index.ts
@@ -1,4 +1,9 @@
 import { CreateTableUsers1683154935617 } from './1683154935617-CreateTableUsers';
 import { CreateTableClients1683156002664 } from './1683156002664-CreateTableClients';
+import { CreateTableProducts1683156837988 } from './1683156837988-CreateTableProducts';
 
-export default [CreateTableUsers1683154935617, CreateTableClients1683156002664];
+export default [
+    CreateTableUsers1683154935617,
+    CreateTableClients1683156002664,
+    CreateTableProducts1683156837988,
+];
